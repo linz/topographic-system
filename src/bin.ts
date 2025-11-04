92c45331a7b7d55ad2daf78b20d979a0c85216d9
ccbblin <205563055+ccbblin@users.noreply.github.com>
2025-11-05 09:23:30 +1300

    remove bin file

--- src/bin.ts
+++
@@ -1,7 +0,0 @@
-import { run } from 'cmd-ts';
-
-import { Cli } from './index';
-
-run(Cli, process.argv.slice(2)).catch((err) => {
-  console.log(err);
-});
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import { subcommands } from 'cmd-ts';
+import { run, subcommands } from 'cmd-ts';
 import { ProduceCommand } from './cli/produce';
 
-export const Cli = subcommands({
+const Cli = subcommands({
   name: 'topographic-system',
   description: '',
   cmds: {
     produce: ProduceCommand
   },
+});
+
+run(Cli, process.argv.slice(2)).catch((err) => {
+  console.log(err);
 });
