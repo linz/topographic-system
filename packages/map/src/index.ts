4485632c57fda580ca7cd90962bcf96f6a8e62fa
Wentao Kuang <wt.kuang@icloud.com>
2025-12-10 10:20:57 +1300

    Rebase to master

--- packages/map/src/index.ts
+++ packages/map/src/index.ts
@@ -1,12 +1,16 @@
 import { run, subcommands } from 'cmd-ts';
 
+import { listMapSheetsCommand } from './cli/action.list.mapsheet.ts';
 import { ProduceCommand } from './cli/action.produce.ts';
+import { downloadCommand } from './cli/action.download.ts';
 
 const Cli = subcommands({
   name: 'topographic-system',
   description: '',
   cmds: {
     produce: ProduceCommand,
+    'list-mapsheets': listMapSheetsCommand,
+    download: downloadCommand,
   },
 });
 
