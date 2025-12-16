18703c476a821ee0f6174a1c9bd57bddd220c8ec
Tobias Schmidt <13055656+schmidtnz@users.noreply.github.com>
2025-12-16 15:28:55 +1300

    test: add global opts

--- packages/kart/src/kart/tests/kart_commands_test.py
+++ packages/kart/src/kart/tests/kart_commands_test.py
@@ -6,9 +6,19 @@ def test_check_kart_command() -> None:
     tests that the kart command is built correctly
     """
     kart_command = get_kart_command(
-        "fetch", ["--option1", "value1", "--option2", "value2"]
+        "fetch",
+        ["--global-opt", "value"],
+        ["--option1", "value1", "--option2", "value2"],
     )
-    assert kart_command == ["fetch", "--option1", "value1", "--option2", "value2"]
+    assert kart_command == [
+        "--global-opt",
+        "value",
+        "fetch",
+        "--option1",
+        "value1",
+        "--option2",
+        "value2",
+    ]
 
 
 def test_check_kart_command_invalid() -> None:
@@ -16,7 +26,7 @@ def test_check_kart_command_invalid() -> None:
     tests that the kart command raises an error when an invalid command is requested
     """
     try:
-        get_kart_command("invalid_command", ["--option1", "value1"])
+        get_kart_command("invalid_command", [], ["--option1", "value1"])
     except ValueError as e:
         assert str(e) == "Unsupported kart command: invalid_command"
     else:
