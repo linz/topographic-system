f172d9b14c8fc94dc878e8db6778af00a905eede
ccbblin <205563055+ccbblin@users.noreply.github.com>
2026-02-11 10:51:56 +1300

    lint

--- packages/data/src/cli/action.ice.contours.ts
+++ packages/data/src/cli/action.ice.contours.ts
@@ -1,7 +1,6 @@
-import { Command } from "@linzjs/docker-command";
-import { logger, toRelative, Url } from "@topographic-system/shared/src/index.ts";
-import { command, option } from "cmd-ts";
-
+import { Command } from '@linzjs/docker-command';
+import { logger, toRelative, Url } from '@topographic-system/shared/src/index.ts';
+import { command, option } from 'cmd-ts';
 
 export const IceContoursArgs = {
   contour: option({
@@ -28,18 +27,18 @@ export const IceContoursCommand = command({
   async handler(args) {
     logger.info({ args }, 'Prepare ice contours: Started');
 
-      const cmd = Command.create('uv');
-      cmd.args.push('run');
-      cmd.args.push('src/ice_contours.py');
-      cmd.args.push(toRelative(args.contour));
-      cmd.args.push(toRelative(args.landcover));
-      cmd.args.push(toRelative(args.output));
-      const res = await cmd.run();
-      logger.debug('ice_contours.py ' + cmd.args.join(' '));
+    const cmd = Command.create('uv');
+    cmd.args.push('run');
+    cmd.args.push('src/ice_contours.py');
+    cmd.args.push(toRelative(args.contour));
+    cmd.args.push(toRelative(args.landcover));
+    cmd.args.push(toRelative(args.output));
+    const res = await cmd.run();
+    logger.debug('ice_contours.py ' + cmd.args.join(' '));
 
-      if (res.exitCode !== 0) {
-        logger.fatal({ ice_contours: res }, 'Failure');
-        throw new Error('ice_contours.py failed to run');
-      }
+    if (res.exitCode !== 0) {
+      logger.fatal({ ice_contours: res }, 'Failure');
+      throw new Error('ice_contours.py failed to run');
+    }
   },
 });
