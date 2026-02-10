775d2deee6ffbf1c548f2df9e05b3189c8e0611a
ccbblin <205563055+ccbblin@users.noreply.github.com>
2026-02-11 10:54:49 +1300

    python format

--- packages/data/src/ice_contours.py
+++ packages/data/src/ice_contours.py
@@ -8,12 +8,14 @@ overlap_path = sys.argv[3]
 contours_gdf = gpd.read_parquet(contour_path)
 landcover_gdf = gpd.read_parquet(landcover_path)
 
-overlap_gdf = gpd.overlay(contours_gdf, landcover_gdf, how='union', keep_geom_type=True)
+overlap_gdf = gpd.overlay(contours_gdf, landcover_gdf, how="union", keep_geom_type=True)
 
-overlap_gdf = overlap_gdf.rename(columns={'feature_type_1': 'feature_type'})
-overlap_gdf = overlap_gdf.rename(columns={'topo_id_1': 'topo_id'})
-overlap_gdf = overlap_gdf.rename(columns={'feature_type_2': 'landcover_feature_type'})
-overlap_gdf = overlap_gdf.rename(columns={'topo_id_2': 'landcover_topo_id'})
-overlap_gdf['landcover_feature_type'] = overlap_gdf['landcover_feature_type'].fillna('other')
+overlap_gdf = overlap_gdf.rename(columns={"feature_type_1": "feature_type"})
+overlap_gdf = overlap_gdf.rename(columns={"topo_id_1": "topo_id"})
+overlap_gdf = overlap_gdf.rename(columns={"feature_type_2": "landcover_feature_type"})
+overlap_gdf = overlap_gdf.rename(columns={"topo_id_2": "landcover_topo_id"})
+overlap_gdf["landcover_feature_type"] = overlap_gdf["landcover_feature_type"].fillna(
+    "other"
+)
 
 overlap_gdf.to_parquet(overlap_path)
