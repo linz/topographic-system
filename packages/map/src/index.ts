b6c56555fd32a4216df0b8192f022c47ce3337b3
Wentao Kuang <wt.kuang@icloud.com>
2026-02-20 11:19:46 +1300

    Also error out for argo runs

--- packages/map/src/index.ts
+++ packages/map/src/index.ts
@@ -1,3 +1,4 @@
+import { isArgo } from '@topographic-system/shared/src/argo.ts';
 import { isGitHubActions } from '@topographic-system/shared/src/env.ts';
 import { run, subcommands } from 'cmd-ts';
 
@@ -18,8 +19,8 @@ const Cli = subcommands({
 run(Cli, process.argv.slice(2)).catch((err) => {
   console.log(err);
 
-  // Only force failure in GitHub Actions
-  if (isGitHubActions) {
+  // Only force failure in GitHub Actions or Argo Workflows, otherwise just log the error for local debugging.
+  if (isGitHubActions || isArgo()) {
     process.exit(1);
   }
 });
