43871c27c663294b0ef3d4bc3f20eb1b6f6a6a22
ccbblin <205563055+ccbblin@users.noreply.github.com>
2026-02-25 14:50:12 +1300

    keep column names from gdal

--- packages/kart/src/cli/action.to.parquet.ts
+++ packages/kart/src/cli/action.to.parquet.ts
@@ -88,7 +88,6 @@ export const parquetCommand = command({
           ['-lco', `COMPRESSION=${args.compression}`],
           ['-lco', `COMPRESSION_LEVEL=${args.compression_level}`],
           ['-lco', `ROW_GROUP_SIZE=${args.row_group_size}`],
-          ['-lco', 'WRITE_COVERING_BBOX=YES'],
         ];
         if (args.sort_by_bbox) {
           command.push(['-lco', 'SORT_BY_BBOX=YES']);
