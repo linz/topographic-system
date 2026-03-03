fc6487a6e90fe44a4d2a244ab11935c7835df0ab
ccbblin <205563055+ccbblin@users.noreply.github.com>
2026-03-04 09:12:31 +1300

    make sure derived_from is in latest collection

--- packages/kart/src/cli/action.contour.landcover.ts
+++ packages/kart/src/cli/action.contour.landcover.ts
@@ -58,17 +58,18 @@ export const ContourWithLandcoverCommand = command({
       throw new Error(`Landcover collection must have a parquet asset: ${args.landcover.toString()}`);
     }
 
-    const latestCollection = await fsa.readJson<StacCollection>(
-      new URL(`${topo50ContourName}/latest/collection.json`, args.output),
-    );
-    if (
-      latestCollection.links.find((link) => link.rel === 'derived_from' && link.href === contourParquetAsset.href) &&
-      latestCollection.links.find((link) => link.rel === 'derived_from' && link.href === landcoverParquetAsset.href)
-    ) {
-      logger.info(
-        'Latest output collection is already up to date with contour and landcover source, skipping processing',
-      );
-      return;
+    const latestCollectionUrl = new URL(`${topo50ContourName}/latest/collection.json`, args.output);
+    if (await fsa.exists(latestCollectionUrl)) {
+      const latestCollection = await fsa.readJson<StacCollection>(latestCollectionUrl);
+      if (
+        latestCollection.links.find((link) => link.rel === 'derived_from' && link.href === contourParquetAsset.href) &&
+        latestCollection.links.find((link) => link.rel === 'derived_from' && link.href === landcoverParquetAsset.href)
+      ) {
+        logger.info(
+          'Latest output collection is already up to date with contour and landcover source, skipping processing',
+        );
+        return;
+      }
     }
 
     const contourParquet = await downloadFile(new URL(contourParquetAsset.href));
@@ -103,7 +104,10 @@ export const ContourWithLandcoverCommand = command({
     logger.info({ assetFile, stacCollectionFile: stacCollectionFile.href }, 'AssetToCollectionUpserted');
 
     logger.debug({ assetFile }, 'UpdatingLatestCollection');
-    await upsertAssetToCollection(assetFile, new URL('../../latest/collection.json', stacCollectionFile));
+    await upsertAssetToCollection(assetFile, new URL('../../latest/collection.json', stacCollectionFile), [
+      derivedFromContour,
+      derivedFromLandcover,
+    ]);
     logger.info('Prepare contour with landcover: Finished');
   },
 });
