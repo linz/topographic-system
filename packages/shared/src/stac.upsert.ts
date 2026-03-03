170c1f48e9bae01e0b22ff469b1e4cd0c6ec24dd
ccbblin <205563055+ccbblin@users.noreply.github.com>
2026-03-04 11:28:30 +1300

    lint and format

--- packages/shared/src/stac.upsert.ts
+++ packages/shared/src/stac.upsert.ts
@@ -1,5 +1,6 @@
-import { fsa } from '@chunkd/fs';
 import { basename } from 'node:path';
+
+import { fsa } from '@chunkd/fs';
 import type { StacCatalog, StacCollection, StacItem, StacLink } from 'stac-ts';
 
 import { CliDate } from './cli.info.ts';
