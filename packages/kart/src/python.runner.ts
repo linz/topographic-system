36d2dbef48c2104a5fcd6fe7244b1f90f6f7b51b
ccbblin <205563055+ccbblin@users.noreply.github.com>
2026-03-05 14:46:25 +1300

    zx fix

--- packages/kart/src/python.runner.ts
+++ packages/kart/src/python.runner.ts
@@ -1,5 +1,3 @@
-import { fileURLToPath } from 'node:url';
-
 import { logger } from '@linzjs/topographic-system-shared';
 import { $ } from 'zx';
 
@@ -7,20 +5,7 @@ import { $ } from 'zx';
  * Running python commands to join contour and landcover
  */
 export async function contourWithLandcover(contour: URL, landcover: URL, output: URL): Promise<void> {
-  const command = [
-    'uv run',
-    '--directory /packages/data-prep',
-    'src/data_prep/contour_with_landcover.py',
-    fileURLToPath(contour),
-    fileURLToPath(landcover),
-    fileURLToPath(output),
-  ];
-
-  const res = await $`${command.join(' ')}`;
-  logger.debug('contour_with_landcover.py ' + command.join(' '));
-
-  if (res.exitCode !== 0) {
-    logger.fatal({ contour_with_landcover: res }, 'Failure');
-    throw new Error('contour_with_landcover.py failed to run');
-  }
+  const res =
+    await $`uv run --directory /packages/data-prep src/data_prep/contour_with_landcover.py ${contour.pathname} ${landcover.pathname} ${output.pathname}`;
+  logger.debug('contour_with_landcover.py ' + res.stdout);
 }
