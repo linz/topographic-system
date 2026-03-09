172f27f3deaca45984dba499c1fb52440825574d
Blayne Chard <bchard@linz.govt.nz>
2026-03-10 10:28:34 +1300

    build: add a simple e2e test for actually making map

--- packages/map/src/cli/action.deploy.ts
+++ packages/map/src/cli/action.deploy.ts
@@ -25,7 +25,9 @@ async function deployAssetsAsTar(projectFolder: URL, tarTargetPath: URL, commit?
   tarPack.pipe(pass);
 
   const projectFiles = await fsa.toArray(fsa.list(projectFolder));
+  console.log({projectFiles})
   if (projectFiles.length === 0) return null;
+  let fileCount = 0;
   for (const file of projectFiles) {
     const filename = basename(file.href);
     if (!filename) throw new Error(`Deploy: Invalid file path ${file.href}`);
@@ -33,7 +35,9 @@ async function deployAssetsAsTar(projectFolder: URL, tarTargetPath: URL, commit?
 
     const data = await fsa.read(file);
     tarPack.entry({ name: filename, size: data.byteLength }, data);
+    fileCount ++;
   }
+  if (fileCount === 0) return null;
 
   tarPack.finalize();
 
