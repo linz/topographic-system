487fbb8de23f4cfb3754eace5e59f16cb0c3c627
Tobias Schmidt <13055656+schmidtnz@users.noreply.github.com>
2026-03-16 16:18:49 +1300

    fix: re-add kart flow command

--- packages/kart/src/index.ts
+++ packages/kart/src/index.ts
@@ -6,12 +6,14 @@ import { CloneCommand } from './cli/action.clone.ts';
 import { ContourWithLandcoverCommand } from './cli/action.contour.landcover.ts';
 import { DiffCommand } from './cli/action.diff.ts';
 import { ExportCommand } from './cli/action.export.ts';
+import { FlowCommand } from './cli/action.flow.ts';
 import { CommentCommand } from './cli/action.pr.comment.ts';
 import { ParquetCommand } from './cli/action.to.parquet.ts';
 import { ValidateCommand } from './cli/action.validate.ts';
 import { VersionCommand } from './cli/action.version.ts';
 
 const cmds = {
+  'kart-flow': FlowCommand,
   clone: CloneCommand,
   diff: DiffCommand,
   export: ExportCommand,
