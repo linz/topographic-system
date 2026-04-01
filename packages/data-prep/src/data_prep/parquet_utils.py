a216152ef230844bf7d60344d6f989d6729ea6af
ccbblin <205563055+ccbblin@users.noreply.github.com>
2026-04-02 11:46:30 +1300

    parquet write row group size

--- packages/data-prep/src/data_prep/parquet_utils.py
+++ packages/data-prep/src/data_prep/parquet_utils.py
@@ -5,9 +5,8 @@ from pathlib import Path
 import geopandas as gpd
 
 
-def write_parquet(gdf: gpd.GeoDataFrame, output: Path):
+def write_parquet(gdf: gpd.GeoDataFrame, output: Path, row_group_size=50000):
     compression_level = 19
-    row_group_size = 10000
 
     gdf.to_parquet(
         output,
