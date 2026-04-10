c1b57c719603b223e390cf9444c290bd7ebc5bab
amcmenamin-linz <amcmenamin@linz.govt.nz>
2026-04-10 14:04:18 +1200

    fix:to_crs

--- packages/data/nztopo50/import/core/pre_processing_steps.py
+++ packages/data/nztopo50/import/core/pre_processing_steps.py
@@ -12,7 +12,7 @@ island_gdf = gpd.read_file(islands)
 island_gdf["location"] = 0
 
 sea_gdf = gpd.read_file(sea_poly)
-sea_gdf = sea_gdf.to_crs(epsg=island_gdf.crs.to_epsg())
+sea_gdf = sea_gdf.to_crs(epsg=2193)
 
 island_Joined_gdf = gpd.sjoin(island_gdf, sea_gdf, how="inner", predicate="intersects")
 island_Joined_gdf["location"] = 1
