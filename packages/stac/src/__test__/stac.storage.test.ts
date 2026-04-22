88977fd7be69da765da3645bdf2f700d7be9e45f
Wentao <WKuang@linz.govt.nz>
2026-04-22 14:36:20 +1200

    update test order as suggested.

--- packages/stac/src/__test__/stac.storage.test.ts
+++ packages/stac/src/__test__/stac.storage.test.ts
@@ -31,9 +31,9 @@ describe('stac.storage', async () => {
       const pusher = new StacPusher(new URL('memory://target/bucket/'), 'data');
       pusher.strategy({ type: 'latest' });
       assert.deepEqual(pusher.strategies, [{ type: 'latest' }]);
-      pusher.strategy({ type: 'commit', commit: 'abc' });
-      assert.deepEqual(pusher.strategies, [{ type: 'commit', commit: 'abc' }, { type: 'latest' }]);
       pusher.strategy({ type: 'date', date: new Date('2024-01-01') });
+      assert.deepEqual(pusher.strategies, [{ type: 'date', date: new Date('2024-01-01') }, { type: 'latest' }]);
+      pusher.strategy({ type: 'commit', commit: 'abc' });
       assert.deepEqual(pusher.strategies, [
         { type: 'date', date: new Date('2024-01-01') },
         { type: 'commit', commit: 'abc' },
