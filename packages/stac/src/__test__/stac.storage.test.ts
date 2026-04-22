ea30681d384df90243a3650e9f3b406fac269791
Wentao <WKuang@linz.govt.nz>
2026-04-22 14:04:53 +1200

    Unit test for strategies priority

--- packages/stac/src/__test__/stac.storage.test.ts
+++ packages/stac/src/__test__/stac.storage.test.ts
@@ -2,6 +2,7 @@ import assert from 'node:assert';
 import { describe, it } from 'node:test';
 
 import { StacStorage } from '../stac.storage.ts';
+import { StacPusher } from '../stac.pusher.ts';
 
 describe('stac.storage', async () => {
   const latest = { type: 'latest' } as const;
@@ -23,5 +24,24 @@ describe('stac.storage', async () => {
         );
       });
     }
+  });
+
+  describe('strategies', () => {
+   it('should sort the strategies by priority', () => {
+      const pusher = new StacPusher(new URL('memory://target/bucket/'), 'data');
+      pusher.strategy({ type: 'latest' });
+      assert.deepEqual(pusher.strategies, [{ type: 'latest' }]);
+      pusher.strategy({ type: 'commit', commit: 'abc' });
+      assert.deepEqual(pusher.strategies, [
+        { type: 'commit', commit: 'abc' },
+        { type: 'latest' },
+      ]);
+      pusher.strategy({ type: 'date', date: new Date('2024-01-01') });
+      assert.deepEqual(pusher.strategies, [
+        { type: 'date', date: new Date('2024-01-01') },
+        { type: 'commit', commit: 'abc' },
+        { type: 'latest' },
+      ]);
+    });
   });
 });
