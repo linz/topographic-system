001f4b761296c1207bbf3660d482cdba5533f439
Blayne Chard <bchard@linz.govt.nz>
2026-05-18 09:46:36 +1200

    refactor: fixup linting

--- packages/kart-import/src/kart_import/uuid/uuid7_test.py
+++ packages/kart-import/src/kart_import/uuid/uuid7_test.py
@@ -2,12 +2,13 @@ import time
 import uuid6
 from .uuid7 import reproducable_uuid7
 
+
 def test_custom_uuidv7():
     ts_ms = int(time.time() * 1000)
     number = 42
-    
+
     custom_uuid = reproducable_uuid7(ts_ms, number)
     assert custom_uuid.version == 7
-    
+
     decoded_uuid = uuid6.UUID(bytes=custom_uuid.bytes)
     assert decoded_uuid.time == ts_ms
