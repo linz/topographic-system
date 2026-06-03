c15e0e100b0512e278cb8fe7fcc4987504b4e289
amcmenamin-linz <amcmenamin@linz.govt.nz>
2026-06-03 14:40:40 +1200

    updated import to handle change

--- packages/data/nztopo50/import/core/kart_import_topodata.bat
+++ packages/data/nztopo50/import/core/kart_import_topodata.bat
@@ -1,84 +1,84 @@
-REM kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id tree_point 
-REM kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id airport 
-kart push origin master 
-REM kart push origin master --force
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id bridge_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id building 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id building_point 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id coastline 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id descriptive_text 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id fence_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id ferry_crossing 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id geographic_name 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id island 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id landcover 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id landcover_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id landcover_point 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id landuse 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id landuse_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id marine 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id physical_infrastructure_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id physical_infrastructure_point 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id place_point 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id railway_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id railway_station 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id relief_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id relief_point 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id relief 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id residential_area 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id road_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id runway 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id structure 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id structure_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id structure_point 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id track_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id transport_point 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id trig_point 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id tunnel_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id vegetation 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id vegetation_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id water 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id water_line 
-kart push origin master
-kart import postgresql://postgres:landinformation@localhost/topo/release64 --primary-key id water_point 
-kart push origin master
-
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id tree_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id airport 
+kart push origin dataload-release66 
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id bridge_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id building 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id building_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id coastline 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id descriptive_text 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id fence_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id ferry_crossing 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id geographic_name 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id island 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id landcover 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id landcover_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id landcover_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id landuse 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id landuse_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id marine 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id physical_infrastructure_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id physical_infrastructure_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id place_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id railway_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id railway_station 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id relief_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id relief_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id residential_area 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id road_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id runway 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id structure 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id structure_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id structure_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id track_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id transport_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id trig_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id tunnel_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id vegetation 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id vegetation_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id water 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id water_line 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id water_point 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id waterway_feature 
+kart push origin dataload-release66
+kart import postgresql://postgres:landinformation@localhost/topo/release66 --replace-existing --primary-key id waterway_feature_line 
+kart push origin dataload-release66
 
 
