cc27c928aa4102a22aabf4ec8fb55121c8909258
amcmenamin-linz <amcmenamin@linz.govt.nz>
2026-08-04 09:19:02 +1200

    support export all tables

--- packages/data/nztopo50/import/utils/export/parquet/export_from_gpkg_geoparquet.py
+++ packages/data/nztopo50/import/utils/export/parquet/export_from_gpkg_geoparquet.py
@@ -1,10 +1,17 @@
 import os
 import geopandas as gpd  # type: ignore
+import pyogrio
 
 # Read the layer from the GeoPackage file
 gpkg_path = r"C:\Data\toposource\topographic-data\topographic-data.gpkg"
 output_path = r"C:\Data\temp"
 layers = ["airport"]
+
+
+if not layers:
+    layers = [layer_name for layer_name, _geometry_type in pyogrio.list_layers(gpkg_path)]
+
+
 
 for layer in layers:
     print(f"Processing layer: {layer}")
