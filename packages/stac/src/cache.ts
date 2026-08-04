b66a3a52483eaf7d5a6b1edeba03c214dafb3cfa
Blayne Chard <blayne@chard.com>
2026-08-04 14:54:32 +1200

    refactor: cleanup control policies

--- packages/stac/src/cache.ts
+++ packages/stac/src/cache.ts
@@ -1,14 +1,17 @@
-export function getAssetCacheControl(target: URL): string {
+export const CacheControl = {
+  // Mutable stac documents and assets change frequently
+  Mutable: 'public, max-age=30',
+  // Immutable stac documents can sometimes change but it is very infrequent
+  StacJson: 'public, max-age=300, stale-while-revalidate=86400',
+  // Immutable assets should never change.
+  Asset: 'public, max-age=31536000, immutable',
+};
+
+export function getCacheControl(target: URL): string {
   const isStac = target.pathname.endsWith('.json');
   const isMutable = target.pathname.includes('/latest/') || target.pathname.includes('/next/');
 
-  if (isStac) {
-    if (isMutable) return 'public, max-age=30';
-    // Immutable stac documents can sometimes change but it is very infrequent
-    return 'public, max-age=300, stale-while-revalidate=86400'
-  }
-
-  if (isMutable) return 'public, max-age=30'
-  // Immutable assets should never change.
-  return 'public, max-age=31536000, immutable'
+  if (isMutable) return CacheControl.Mutable;
+  if (isStac) return CacheControl.StacJson;
+  return CacheControl.Asset;
 }
